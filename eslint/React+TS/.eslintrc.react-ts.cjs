3b67e2a7046addabb107f66f81c50514f3caaf38
xwdrblue <61836229+hugo38rodrigues@users.noreply.github.com>
2026-02-05 11:55:01 +0100

    Update config for eslint 9

--- eslint/React+TS/.eslintrc.react-ts.cjs
+++ eslint/React+TS/.eslintrc.react-ts.cjs
@@ -1,81 +1,104 @@
-/* eslint-disable no-undef */
-module.exports = {
-  root: true,
-  env: { browser: true, es2021: true },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true, // ou ["apps/web/tsconfig.json"] etc.
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: { jsx: true },
-  },
-  extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "airbnb-typescript",
-  ],
-  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y", "import"],
-  settings: {
-    react: { version: "detect" },
-    "import/resolver": {
-      typescript: {},
-      node: { extensions: [".js", ".ts", ".tsx", ".d.ts"] },
-    },
-  },
-	rules: {
-		// Style
-		"semi": ["error", "never"],
-		"@typescript-eslint/semi": ["error", "never"],
-		"indent": ["error", "tab"],
-		"no-tabs": "off",
-		"jsx-quotes": ["error", "prefer-single"],
+import js from '@eslint/js'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
 
-		// Imports
-		"import/prefer-default-export": "off",
-		"import/extensions": ["error", "ignorePackages", { js: "never", mjs: "never", ts: "never", tsx: "never" }],
-
-		// React
-		"react/react-in-jsx-scope": "off",
-		"react/jsx-props-no-spreading": "off",
-		"react/require-default-props": "off",
-		"react/prop-types": "off",
-		"react/no-array-index-key": "off",
-		"react/function-component-definition": [
-			"error",
-			{ namedComponents: "arrow-function", unnamedComponents: "arrow-function" }
-		],
-		"react/jsx-indent": ["error", "tab"],
-		"react/jsx-indent-props": ["error", "tab"],
-
-		// TS
-		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-		"@typescript-eslint/explicit-function-return-type": "off",
-
-		// Divers
-		"class-methods-use-this": "off",
-		"no-console": ["warn", { allow: ["warn", "error"] }],
-		"no-useless-escape": "off",
-		"no-underscore-dangle": ["error", { allow: ["_id"] }], // If Database is MongoDB
-
-		// Longueur & style
-		"max-len": ["warn", { code: 120, ignoreStrings: true, ignoreTemplateLiterals: true, ignoreComments: true }],
-		"arrow-body-style": "off",
+export default [
+	js.configs.recommended,
+	{
+		ignores: ['node_modules', '.next', 'out', 'dist'],
 	},
+	{
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				project: true,
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				ecmaFeatures: { jsx: true },
+			},
+			globals: {
+				document: 'readonly',
+				window: 'readonly',
+				navigator: 'readonly',
+				console: 'readonly',
+				process: 'readonly',
+				React: 'readonly',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': typescript,
+			'react': react,
+			'react-hooks': reactHooks,
+			'jsx-a11y': jsxA11y,
+			'import': importPlugin,
+		},
+		settings: {
+			react: { version: 'detect' },
+			'import/resolver': {
+				typescript: {},
+				node: { extensions: ['.js', '.ts', '.tsx', '.d.ts'] },
+			},
+		},
+		rules: {
+			// Style
+			'semi': ['error', 'never'],
+			'@typescript-eslint/semi': ['error', 'never'],
+			'indent': ['error', 'tab'],
+			'no-tabs': 'off',
+			'jsx-quotes': ['error', 'prefer-single'],
 
-  overrides: [
-    {
-      files: ["**/*.{test,spec}.{ts,tsx}"],
-      plugins: ["vitest"],
-      rules: {
-        "no-unused-expressions": "off",
-      },
-    },
-    {
-      files: ["**/*.d.ts"],
-      rules: {
-        "import/no-duplicates": "off",
-        "spaced-comment": "off",
-      },
-    },
-  ],
-};
+			// Imports
+			'import/prefer-default-export': 'off',
+			'import/extensions': ['error', 'ignorePackages', { js: 'never', mjs: 'never', ts: 'never', tsx: 'never' }],
+
+			// React
+			'react/react-in-jsx-scope': 'off',
+			'react/jsx-props-no-spreading': 'off',
+			'react/require-default-props': 'off',
+			'react/prop-types': 'off',
+			'react/no-array-index-key': 'off',
+			'react/function-component-definition': [
+				'error',
+				{ namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+			],
+			'react/jsx-indent': ['error', 'tab'],
+			'react/jsx-indent-props': ['error', 'tab'],
+
+			// React Hooks
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn',
+
+			// TypeScript
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'no-unused-vars': 'off',
+
+			// Divers
+			'class-methods-use-this': 'off',
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			'no-useless-escape': 'off',
+			'no-underscore-dangle': ['error', { allow: ['_id'] }],
+
+			// Longueur & style
+			'max-len': ['warn', { code: 120, ignoreStrings: true, ignoreTemplateLiterals: true, ignoreComments: true }],
+			'arrow-body-style': 'off',
+		},
+	},
+	{
+		files: ['**/*.{test,spec}.{ts,tsx}'],
+		rules: {
+			'no-unused-expressions': 'off',
+		},
+	},
+	{
+		files: ['**/*.d.ts'],
+		rules: {
+			'import/no-duplicates': 'off',
+		},
+	},
+]
